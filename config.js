635cf3b4bc314724ad4c773f695189856daf7707
Tilman May <78152259+TilmanMay@users.noreply.github.com>
2024-01-26 12:59:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token:"ODAzMzU5NDIyMzIzMjI4NzEz.YA8oww.1DGfVwZtM8ZNH9f7iEkFxSlMjo0",
+  token:"Hier würde ein Token stehen",
   folder:"walross_bot/voicerecords",
   master:[
     "803547586110816268"
